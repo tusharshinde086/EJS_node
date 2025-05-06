9da65a97232f8df80154fa163cbb5a72e1bde574
Tushar Shinde <tusharshindets086@gmail.com>
2025-05-06 14:13:54 +0530

    passing data to  EJS rolldice

--- index.js
+++ index.js
@@ -7,18 +7,19 @@ const port = 8080;
 app.set("view engin ", "ejs ");
 app.set("views",path.join(__dirname , "/views"));
 
-app.get("/", (req , res )=> {
-    // res.send("this is root ");
+// app.get("/", (req , res )=> {
+//     // res.send("this is root ");
 
-});
+//     res.render("home.ejs");
+// });
+
 app.get("/hello ", (req , res )=> {
     res.send("hello  ! ");
 
 });
-app.get("/rolldice ", (req , res )=> {
-    
+app.get("/ ", (req , res )=> {
+     
     res.render("rolldice.ejs");
-
 
 });
 app.listen(port,() => {
