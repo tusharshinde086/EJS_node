3a9808aa0e622f74e35ffbe23f33408cfedef031
Tushar Shinde <tusharshindets086@gmail.com>
2025-05-06 14:28:55 +0530

    instagram ejs    ligl:username

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 const express = require("express");
 const app = express();
-const path =require("path");
+const path = require("path");
 const port = 8080;
 
+// Set EJS as the templating engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
-app.set("view engin ", "ejs ");
-app.set("views",path.join(__dirname , "/views"));
-
-app.get("/", (req , res )=> {
-    // res.send("this is root ");
-
+// Root route
+app.get("/", (req, res) => {
     res.render("home.ejs");
 });
 
-app.get("/hello", (req , res )=> {
-    res.send("hello  ! ");
-
+// Dynamic route for Instagram-like username
+app.get("/ig/:username", (req, res) => {
+    let { username } = req.params;
+    console.log(username);
+    res.send(`Instagram profile for user: ${username}`);
 });
-app.get("/rolldice", (req , res )=> {
-     let dicevalue = Math.floor(Math.random() * 6) + 1 ;
-    res.render("rolldice.ejs", {dicevalue });
 
+// Simple hello route
+app.get("/hello", (req, res) => {
+    res.send("hello!");
 });
-app.listen(port,() => {
-    console.log(`listening on port ${port}`);
+
+// Dice roll route
+app.get("/rolldice", (req, res) => {
+    let dicevalue = Math.floor(Math.random() * 6) + 1;
+    res.render("rolldice.ejs", { dicevalue });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
