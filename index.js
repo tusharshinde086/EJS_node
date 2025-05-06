704657823d67b8e7f7edd34b326f54ecdedce7f2
Tushar Shinde <tusharshindets086@gmail.com>
2025-05-06 14:12:50 +0530

    passing data to  EJS rolldice

--- index.js
+++ index.js
@@ -10,13 +10,17 @@ app.set("views",path.join(__dirname , "/views"));
 app.get("/", (req , res )=> {
     // res.send("this is root ");
 
-    res.render("home.ejs");
 });
 app.get("/hello ", (req , res )=> {
     res.send("hello  ! ");
 
 });
+app.get("/rolldice ", (req , res )=> {
+    
+    res.render("rolldice.ejs");
 
+
+});
 app.listen(port,() => {
     console.log(`listening on port ${port}`);
 });
