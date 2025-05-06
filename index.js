8716d5319eaa2b124e87dc1ea7d429c169c6ec38
Tushar Shinde <tusharshindets086@gmail.com>
2025-05-06 13:41:19 +0530

    path join

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const app = express();
+const path =require("path");
 const port = 8080;
 
+
 app.set("view engin ", "ejs ");
+app.set("views",pathjoin(__dirname , "/views"));
+
 app.get("/", (req , res )=> {
     // res.send("this is root ");
 
     res.render("home.ejs");
-})
+});
 app.get("/hello ", (req , res )=> {
     res.send("hello  ! ");
 
-})
+});
 
 app.listen(port,() => {
     console.log(`listening on port ${port}`);
-})
+});
