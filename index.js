929e33d2b0950a8240ffad24e0b9d2abf92edfa5
Tushar Shinde <tusharshindets086@gmail.com>
2025-05-06 13:26:58 +0530

    using ejs

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+
+app.set("view engin ", "ejs ");
+app.get("/", (req , res )=> {
+    // res.send("this is root ");
+
+    res.render("home.ejs");
+})
+
 app.listen(port,() => {
     console.log(`listening on port ${port}`);
 })
