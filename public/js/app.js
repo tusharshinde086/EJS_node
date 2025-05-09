91becd322bf7f8bf08fe61609d3644ceb978c5f0
Tushar Shinde <tusharshindets086@gmail.com>
2025-05-09 19:57:17 +0530

    js

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 const btns = document.querySelectorAll("button");
-for(btn of btns){
-     btn.addEventListener("click",{} => {
+for (let btn of btns) {
+    btn.addEventListener("click", () => {
         console.log("button was clicked");
-     });
+    });
 }
